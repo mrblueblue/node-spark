a2f67a4b55fbba61c2cf52d8121db8b424581f70
Jonathan Huang <grokut@gmail.com>
2015-03-15 11:00:34 -0700

    tabs to spaces

--- turn.js
+++ turn.js
@@ -4,41 +4,41 @@ var red = function(err,device){
   device.callFunction('allLedsOn', 'red', function(err, data) {
     if (err) {
       console.log('An error occurred:', err);
-	  } else {
+    } else {
       console.log('red rum:', data);
     }
-	});
+  });
 };
 
 var blue = function(err,device){ 
   device.callFunction('allLedsOn', 'blue', function(err, data) {
-	  if (err) {
-	    console.log('An error occurred:', err);
-	  } else {
-	    console.log('blue moon:', data);
-	  }
-	});
+    if (err) {
+      console.log('An error occurred:', err);
+    } else {
+      console.log('blue moon:', data);
+    }
+  });
 };
 
 
 var rainbow = function(err,device){
   device.callFunction('allLedsOn', 'rain', function(err, data) {
-	  if (err) {
-	    console.log('An error occurred:', err);
-	  } else {
-	    console.log('rainbow gathering:', data);
-	  }
-	});
+    if (err) {
+      console.log('An error occurred:', err);
+    } else {
+      console.log('rainbow gathering:', data);
+    }
+  });
 }
 
 var off = function(err,device){
   device.callFunction('allLedsOff', 'off', function(err, data) {
-	  if (err) {
-	    console.log('An error occurred:', err);
-	  } else {
-	    console.log('off kilter:', data);
-	  }
-	});
+    if (err) {
+      console.log('An error occurred:', err);
+    } else {
+      console.log('off kilter:', data);
+    }
+  });
 }
 
 
